4a8b7ffe4c268cc950f7b4f89abbc315f81c7a66
Bryan de Jong <bryandejong99@gmail.com>
2022-06-01 14:33:50 +0200

    fix: remove den haag react button

--- src/components/ButtonsToolbar/index.js
+++ src/components/ButtonsToolbar/index.js
@@ -3,11 +3,9 @@ import PropTypes from 'prop-types';
 
 import {Literal, LiteralsProvider} from 'components/Literal';
 import {Toolbar, ToolbarList} from 'components/Toolbar';
-import {Button as DHButton} from "@gemeente-denhaag/button";
 import Loader from 'components/Loader';
 import LogoutButton from 'components/LogoutButton';
 import {SUBMISSION_ALLOWED} from 'components/constants';
-import {ArrowRightIcon} from "@gemeente-denhaag/icons";
 
 const ButtonsToolbar = ({literals, canSubmitStep, canSubmitForm, loginRequired, isAuthenticated, isLastStep, isCheckingLogic, onNavigatePrevPage, onFormSave, onLogout}) => {
   const showSubmitButton = !(canSubmitForm === SUBMISSION_ALLOWED.noWithoutOverview && isLastStep);
@@ -26,27 +24,39 @@ const ButtonsToolbar = ({literals, canSubmitStep, canSubmitForm, loginRequired, 
           <ToolbarList>
             {
               showSubmitButton
-              && (<DHButton
+              && (<button
                 type="submit"
-                variant="primary"
                 name="next"
                 disabled={!canSubmitStep}
-                icon={<ArrowRightIcon />}
-                iconAlign="end"
+                className={"denhaag-button denhaag-button--end-icon"}
               >
                 {
                   isCheckingLogic
                     ? (<Loader modifiers={['centered', 'only-child', 'small']}/>)
                     : (<Literal name="nextText"/>)
                 }
-              </DHButton>)
+                <span className={"denhaag-button__icon"}>
+                  <svg
+                    aria-hidden="true"
+                    className="denhaag-icon"
+                    focusable="false"
+                    height="1em"
+                    viewBox="0 0 24 24"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M12.293 5.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L16.586 13H5a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"/>
+                  </svg>
+                </span>
+              </button>)
             }
-            <DHButton
+            <button
               type="button"
-              variant="secondary-action"
+              className={"denhaag-button denhaag-button--primary-action"}
               name="save"
               onClick={onFormSave}
-            ><Literal name="saveText"/></DHButton>
+            ><Literal name="saveText"/></button>
           </ToolbarList>
         </Toolbar>
       </LiteralsProvider>
